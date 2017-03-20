79c4fa402b23ed6593d5121af62b7903ace520ee
Tugrul Can <tugrulcansollu@gmail.com>
2017-03-20 23:43:51 +0300

    Added StackUsingLinkedList

---
+++ StackImplementation/StackUsingLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementation
+{
+    public class StackUsingLinkedList : IStack
+    {
+        public object Top { get; set; }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(object item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- StackUnitTestProject/StackUsingLinkedListUnitTests.cs
+++ StackUnitTestProject/StackUsingLinkedListUnitTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace StackWithLinkedListUnitTests
+namespace StackUsingLinkedListUnitTests
 {
     [TestClass]
     public class UnitTest
