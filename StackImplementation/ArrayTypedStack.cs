0e19d7dc23ad9445d15deb8b9f501013239f4f4f
Tugrul Can <tugrulcansollu@gmail.com>
2017-03-21 01:40:39 +0300

    Added ArrayTypedStack class

---
+++ StackImplementation/ArrayTypedStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementation
+{
+    public class ArrayTypedStack : IStack
+    {
+
+        public int Size { get; set; }
+        private object[] items_array;
+
+        public object Top { get; set; }
+        public ArrayTypedStack(int array_size)
+        {
+            this.Top = -1;
+            this.Size = 0;
+            items_array = new object[array_size];
+
+        }
+
+        public string DisplayElements()
+        {
+            if (this.IsEmpty())
+                throw new IndexOutOfRangeException();
+            else
+            {
+                string result = "";
+                for (int i = (int)this.Top; i > 0; i--)
+                {
+                    result += " " + items_array[i].ToString();
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(object item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
